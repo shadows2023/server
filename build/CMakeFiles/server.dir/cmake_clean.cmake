165b23f76af21dd3dbae8e0e2b2d1e498a0fcc2b
shadows2023 <18858122027@163.com>
2026-04-14 06:28:49 +0000

    上传makefile

---
+++ build/CMakeFiles/server.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "/home/star/server/bin/out/server"
+  "/home/star/server/bin/out/server.pdb"
+  "CMakeFiles/server.dir/src/hello.cpp.o"
+  "CMakeFiles/server.dir/src/hello.cpp.o.d"
+  "CMakeFiles/server.dir/src/main.cpp.o"
+  "CMakeFiles/server.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/server.dir/src/main.cpp.o.d
@@ -0,0 +1,2 @@
+CMakeFiles/server.dir/src/main.cpp.o: /home/star/server/src/main.cpp \
+ /usr/include/stdc-predef.h /home/star/server/inc/hello.h
